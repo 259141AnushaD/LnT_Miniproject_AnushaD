82f388a49de5d82a6ad34aab27ff7658896eb55d
259141AnushaD <54492524+259141AnushaD@users.noreply.github.com>
2021-04-16 12:37:30 +0530

    Update balance_check.c

--- 3_Implementation/Src files/balance_check.c
+++ 3_Implementation/Src files/balance_check.c
@@ -1,1 +1,12 @@
-ban
+#include"atm service.h"
+/**
+ * @brief Construct a new balance object
+ * 
+ * @param amount 
+ */
+
+void checkBalance(float balance)
+{
+    printf("You Choose to See your Balance\n");
+    printf("\n\n****Your Available Balance is:   $%.2f\n\n", balance);
+}
