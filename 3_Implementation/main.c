1f952276f64be0f250a47e86fdd33727b79289ad
259141AnushaD <54492524+259141AnushaD@users.noreply.github.com>
2021-04-16 13:05:51 +0530

    Update main.c

--- 3_Implementation/main.c
+++ 3_Implementation/main.c
@@ -1,4 +1,12 @@
 #include<stdio.h>
+// #include "atm service.h"
+void login();
+void mainMenu();
+void checkBalance(float balance);
+float moneyDeposit(float balance);
+float moneyWithdraw(float balance);
+void menuExit();
+void errorMessage();
 
 int main(){
   //local declarations
@@ -49,3 +57,25 @@ int main(){
   }
   return 0;
 }
+void mainMenu() {
+    
+    printf("******************Hello!*******************\n");
+    printf("**********Welcome to ATM Banking***********\n\n");
+    printf("****Please choose one of the options below****\n\n");
+    printf("< 1 >  Check Balance\n");
+    printf("< 2 >  Deposit\n");
+    printf("< 3 >  Withdraw\n");
+    printf("< 4 >  Exit\n\n");
+    
+}
+void menuExit() {
+    printf("--------------Take your receipt!!!------------------\n");
+    printf("-----Thank you for using ATM Banking Machine!!!-----\n");
+    printf("-----BROUGHT TO YOU BY code-projects.org-----\n");
+    
+    
+}//exit menu
+
+void errorMessage() {;
+    printf("+++!!!You selected invalid number!!!+++\n");
+}//error message
