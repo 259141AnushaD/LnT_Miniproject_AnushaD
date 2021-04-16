b583349c4bea6c8e8b5525422578ea31ba9a20ed
259141AnushaD <54492524+259141AnushaD@users.noreply.github.com>
2021-04-16 13:00:45 +0530

    Update money_withdraw.c

--- 3_Implementation/Src files/money_withdraw.c
+++ 3_Implementation/Src files/money_withdraw.c
@@ -1,1 +1,40 @@
-
+#include"atm service.h"
+/**
+ * @brief 
+ * 
+ * @param amount 
+ * @param amountw 
+ * @return int 
+ */
+float moneyWithdraw(float balance) {
+    float withdraw;
+    bool back = true;
+    
+    printf("You choose to Withdraw a money\n");
+    printf("$$$$Your Balance is: $%.2f\n\n", balance);
+    
+    while (back) {
+    printf("Enter your amount to withdraw:\n");
+    scanf("%f", &withdraw);
+    
+    
+    if (withdraw < balance) {
+        back = false;
+        balance -= withdraw;
+        printf("\n$$$$Your withdrawing money is:  $%.2f\n", withdraw);
+        printf("****Your New Balance is:   $%.2f\n\n", balance);
+        
+    }
+    
+        else  {
+        
+        printf("+++You don't have enough money+++\n");
+        printf("Please contact to your Bank Customer Services\n");
+        printf("****Your Balance is:   $%.2f\n\n", balance);
+    
+    }
+    }
+    return balance;
+    
+    
+}
